0a2b579275f7c10db6b8a877660e0c2532ee5b48
emilieez <emilie.m.zhang@gmail.com>
2018-11-05 17:15:27 -0800

    Event details style change

--- components/EventDetailsHiddenItemsComponent.js
+++ components/EventDetailsHiddenItemsComponent.js
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-around',
-        padding: 20,
+        padding: 15,
     },
     textDetailsContainer: {
         width: SCREEN_WIDTH * 0.85,
