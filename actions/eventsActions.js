94d7b449a82c39faad366749aed4ebbae11b2295
emilieez <emilie.m.zhang@gmail.com>
2018-11-23 14:26:34 -0800

    Create event works

--- actions/eventsActions.js
+++ actions/eventsActions.js
@@ -1,4 +1,4 @@
-import { SONGKICK_API_KEY } from '../constants/ApiServices';
+import {SONGKICK_API_KEY} from '../constants/ApiServices';
 import * as ActionType from './index';
 
 export const confirmEvent = eventIndex => ({
@@ -6,9 +6,34 @@ export const confirmEvent = eventIndex => ({
     payload: eventIndex
 });
 
+//ec2-34-221-16-108.us-west-2.compute.amazonaws.com/event?
+// owner_id=205&name=RayCreatedTest&detail=RayDetail&start=2018-11-17T06:00:00.000Z
+// &end=2018-14-17T10:00:00.000Z&location_name=23157–23333 Billy Brown Rd, Langley
+// &lat=49.171452&long=-122.5791
 export const createEvent = event => ({
-    type: 'CREATE_EVENT',
-    payload: event
+    type: ActionType.CREATE_EVENT,
+    payload: {
+        client: 'rendevous',
+        request: {
+            method: 'POST',
+            url: '/event',
+            headers: {
+                'Content-Type': 'application/json',
+                'api-key': 'j05wd2ae49d212578ef13cb607cef64b',
+                'sessiontoken': 'Kw/xlaGwyV/6mmf6CF2oxo9Y4eqokxzO'
+            },
+            data: {
+                "owner_id": event.ownerid,
+                "name": event.title,
+                "detail": event.eventInfo,
+                "start": event.startDatetime,
+                "end": event.endDatetime,
+                "location_name": event.location,
+                "lat": 49.171452,
+                "long": -122.5791
+            }
+        }
+    }
 });
 
 export const addToSongkickEvents = events => ({
@@ -18,13 +43,12 @@ export const addToSongkickEvents = events => ({
 
 export const getSongkickEvents = () => {
     return {
-      type: ActionType.GET_SONGKICK_EVENTS,
-      payload: {
-        client: 'songkick',
-        request: {
-          url: `/events.json?apikey=${SONGKICK_API_KEY}&location=geo:49.286590,-123.115830`
+        type: ActionType.GET_SONGKICK_EVENTS,
+        payload: {
+            client: 'songkick',
+            request: {
+                url: `/events.json?apikey=${SONGKICK_API_KEY}&location=geo:49.286590,-123.115830`
+            }
         }
-      }
     };
-  }
-  
+}
--- reducers/EventsReducer.js
+++ reducers/EventsReducer.js
@@ -4,6 +4,7 @@ import {FAILURE, REQUEST, SUCCESS} from '../constants/Action-Type';
 const eventStates = {
     availableEvents: [],
     confirmedEvents: [],
+    myEvents: [],
     loading: false
 };
 
@@ -12,7 +13,7 @@ export const eventsReducer = (state = eventStates, action) => {
     const {
         availableEvents,
         confirmedEvents,
-        songKickEvents
+        myEvents
     } = state;
 
     switch (action.type) {
@@ -35,37 +36,22 @@ export const eventsReducer = (state = eventStates, action) => {
                 loading: false,
                 error: 'Error while fetching songkick events'
             }
+        case REQUEST(ActionType.CREATE_EVENT):
+            return {
+                ...state,
+            };
+        case SUCCESS(ActionType.CREATE_EVENT):
+            let newEvent = action.payload.data.params;
+            newEvent['isCurrentUserHost'] = true;
+            confirmedEvents.push(newEvent);
+            return {
+                ...state
+            };
         case 'CONFIRM_EVENT':
             const confirmedEvent = availableEvents[action.payload];
-            // confirmedEvent.eventConfirmed = true;
             confirmedEvents.push(confirmedEvent);
             availableEvents.splice(action.payload, 1);
             console.log(availableEvents);
-            return state;
-
-        // case 'SONGKICK_EVENT':
-        //     availableEvents.push(action.payload);
-        //     return state;
-
-        case 'CREATE_EVENT':
-            const submittedEvent = action.payload;
-            let confirmed = {};
-            confirmed.eventHostName = 'Johnny';
-            confirmed.eventTitle = submittedEvent.title;
-            confirmed.eventDescription = submittedEvent.eventInfo;
-            confirmed.eventDay = submittedEvent.startDayOfWeek;
-            confirmed.eventDate = submittedEvent.startDate;
-            confirmed.eventTime = submittedEvent.startTime;
-            confirmed.eventHostPhoto = require('../assets/Pngs/profilePhoto.imageset/profilePhoto.png');
-            confirmed.guestNums = 'No guests yet';
-            confirmed.eventAway = 2.5;
-            confirmed.eventAddress = submittedEvent.location;
-            confirmed.eventWebsite = 'www.website.com';
-            confirmed.eventConfirmed = true;
-            confirmed.isCurrentUserHost = true;
-
-            confirmedEvents.push(confirmed);
-
             return state;
 
         default:
