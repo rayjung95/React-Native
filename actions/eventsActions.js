6ae5b0664133b82a2a90d0ce4da23d11c8b02555
emilieez <emilie.m.zhang@gmail.com>
2018-11-23 14:37:29 -0800

    Delete comment

--- actions/eventsActions.js
+++ actions/eventsActions.js
@@ -6,10 +6,6 @@ export const confirmEvent = eventIndex => ({
     payload: eventIndex
 });
 
-//ec2-34-221-16-108.us-west-2.compute.amazonaws.com/event?
-// owner_id=205&name=RayCreatedTest&detail=RayDetail&start=2018-11-17T06:00:00.000Z
-// &end=2018-14-17T10:00:00.000Z&location_name=23157–23333 Billy Brown Rd, Langley
-// &lat=49.171452&long=-122.5791
 export const createEvent = event => ({
     type: ActionType.CREATE_EVENT,
     payload: {
